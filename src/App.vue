cd6a25af0cce142130be7a7711348df9e814a897
hoseinx <hoseinx@gmail.com>
2025-08-27 09:15:09 +0330

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <h1>{{ $t("hello") }}</h1>
+        <p>{{ $t("welcome") }}</p>
+
+        <!-- دکمه تغییر زبان -->
+        <v-btn @click="switchLang('fa')">فارسی</v-btn>
+        <v-btn @click="switchLang('en')">English</v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
+
+const { locale } = useI18n();
+
+function switchLang(lang: string) {
+  locale.value = lang;
+}
+</script>
---
+++ src/main.ts
@@ -0,0 +1,35 @@
+// main.ts
+import { createApp } from "vue";
+import { createVuetify } from "vuetify";
+import App from "./App.vue";
+import "vuetify/styles";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+// i18n
+import { createI18n } from "vue-i18n";
+import en from "../locales/en.json";
+import fa from "../locales/fa.json";
+
+// ✅ i18n config
+const i18n = createI18n({
+  locale: "fa", // زبان پیشفرض
+  fallbackLocale: "en",
+  messages: {
+    en,
+    fa,
+  },
+});
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: { mdi },
+  },
+});
+
+createApp(App).use(vuetify).use(i18n).mount("#app");
